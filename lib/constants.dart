1da69e26cfa91d85dd1eb5b5583a2ecdb0b45e95
Game Gem <gamegemcoin@gmail.com>
2026-03-18 16:47:18 +0400

    l

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   // Chrome (Web) ke liye localhost use hota hai.
-  static const String baseUrl = 'https://jackpot-zz3c.onrender.com';
+  static const String baseUrl = 'https://jackpot-zz3c.onrender.com/api';
 }
